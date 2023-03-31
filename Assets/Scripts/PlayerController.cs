b5c3270473f6de694c6e661d4a1e04beb8294eb7
tekinteks@gmail.com <tekinteks@gmail.com>
2023-03-31 21:35:45 +0300

    Boundaries and constraints added to the movement of the player. ForceMode.Force added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed =1.0f;
+    private float speed = 3.0f;
     private Rigidbody playerRb;
+    private float zBound = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,17 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        playerRb.AddForce(Vector3.forward * speed * verticalInput);
-        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput, ForceMode.Force);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput, ForceMode.Force);
+
+        if (transform.position.z > zBound )
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
     }
 }
